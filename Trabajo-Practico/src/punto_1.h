3a5dc1429bc56adfbeb13e69458079cbf7636345
ValentinProvostFIUBA <vprovost@fi.uba.ar>
2024-06-21 15:35:59 -0300

    punto 1 con pre y post

--- Trabajo-Practico/src/punto_1.h
+++ Trabajo-Practico/src/punto_1.h
@@ -5,28 +5,32 @@
 #include <string>
 
 /*
- * Verifica si el texto ingresado por el usuario es valido para busqueda.
- * Devuelve true si es valido o false caso contrario.
- */
+*PRE: -
+*POST: verifica si el texto ingresado por el usuario es valido para busqueda, si es valido devuelve true, sino false.
+*/
+
 bool verificarIngresoValido (std::string barrioPorBuscar);
 
 /*
- *Funcion auxiliar que transforma todos los caracteres de un flujo de
- *	cadena a minusculas.
- */
+*PRE: - 
+*POST: transforma todas las letras de la palabra ingresada a minusculas.
+*/
+
 void transformarMinusculas (std::string &palabraATransformar);
 
 /*
- * Funcion que recibe como parametro un puntero a un puntero de barrios y
- * el nombre que el usuario ingreso.
- * Busca en toda la lista el barrio buscado y lo devuelve como resultado.
- */
+* PRE: recibe como parametro un puntero a un puntero de barrios y
+* el nombre que el usuario ingreso.
+* POST: busca en toda la lista el barrio buscado y lo devuelve como resultado.
+*/
+
 Barrio * buscarBarrio (std::string &nombreBarrio, Lista<Barrio *> * barrios);
 
 /*
- * Funcion que recibe como parametro un puntero a puntero de barrios.
- * Devuelve el Barrio buscado.
- */
+*PRE: recibe como parametro un puntero a puntero de barrios.
+*POST: Devuelve el Barrio buscado.
+*/
+
 Barrio * mostrarPorBarrio(Lista<Barrio *> * barrios, std::string barrioPorBuscar);
 
 #endif //PUNTO_1_H
