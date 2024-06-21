1c1f311e9018a59b458fcc94b8020c96a69d66f7
Jhoel Ushiña <bushina@fi.uba.ar>
2024-06-21 03:41:02 -0300

    adicion punto1

---
+++ Trabajo-Practico/src/punto_1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jhoel06 on 6/20/24.
+//
+
+#include "punto_1.h"
---
+++ Trabajo-Practico/src/punto_1.h
@@ -0,0 +1,8 @@
+//
+// Created by jhoel06 on 6/20/24.
+//
+
+#ifndef PUNTO_1_H
+#define PUNTO_1_H
+
+#endif //PUNTO_1_H
